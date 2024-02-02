028460919e760614425018c4f87b693be300f0aa
Adam Zerner <adamzerner@protonmail.com>
2024-02-01 18:57:51 -0800

    Use `getStylesheetUrl` and place after `styles.css`

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,8 +8,8 @@ export default function App({ Component }: PageProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>fresh-rfui-test</title>
-        <link rel="stylesheet" href="https://rfui.deno.dev/rfui-v0-1-11.css" />
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="stylesheet" href={getStylesheetUrl()} />
       </head>
       <body>
         <Component />
